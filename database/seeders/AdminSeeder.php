55beab87bb76d2ddc3a925ff3a480a9dab012702
abdulmannans <samannan1999@gmail.com>
2025-05-17 05:06:06 +0530

    added admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        User::truncate();
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@wls.com',
+            'password' => bcrypt('secret123'),
+        ]);
+    }
+}
